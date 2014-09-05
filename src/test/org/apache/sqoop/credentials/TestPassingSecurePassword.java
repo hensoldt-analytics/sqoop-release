5fcfd4a806ee70d3dbbdbc3d0e984ee7c02d1444
Venkat Ranganathan <venkat@hortonworks.com>
2014-09-04 22:38:56 -0700

    Fix for BUG-22310: Use File url for test provider on Windows

--- src/test/org/apache/sqoop/credentials/TestPassingSecurePassword.java
+++ src/test/org/apache/sqoop/credentials/TestPassingSecurePassword.java
@@ -374,8 +374,10 @@ public class TestPassingSecurePassword extends BaseSqoopTestCase {
       File credDir = new File(".");
 
       Configuration conf = getConf();
-      String ourUrl =  CredentialProviderHelper.SCHEME_NAME +
-        "://file/" + credDir.getAbsolutePath() + "/" + jksFile;
+      String ourUrl =  CredentialProviderHelper.SCHEME_NAME
+          + "://file/" + credDir.getAbsolutePath().replaceAll("\\\\", "/")
+          + "/" + jksFile;
+
       File file = new File(credDir, jksFile);
       file.delete();
       conf.set(CredentialProviderHelper.CREDENTIAL_PROVIDER_PATH,
@@ -415,8 +417,10 @@ public class TestPassingSecurePassword extends BaseSqoopTestCase {
       File credDir = new File(".");
 
       Configuration conf = getConf();
-      String ourUrl =  CredentialProviderHelper.SCHEME_NAME +
-        "://file/" + credDir.getAbsolutePath() + "/" + jksFile;
+      String ourUrl =  CredentialProviderHelper.SCHEME_NAME
+        + "://file/" + credDir.getAbsolutePath().replaceAll("\\\\", "/")
+        + "/" + jksFile;
+
       File file = new File(credDir, jksFile);
       file.delete();
       conf.set(CredentialProviderHelper.CREDENTIAL_PROVIDER_PATH,
